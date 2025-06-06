8bf1f8d100421376ed1e43fab36676fe85ffee7c
AmEl <amirelkanov0504@gmail.com>
2025-06-06 23:54:53 +0300

    lets try out our ci cd

--- yandex-cloud-tf/outputs.tf
+++ yandex-cloud-tf/outputs.tf
@@ -3,15 +3,15 @@ output "registry_id" {
   value       = yandex_container_registry.registry.id
 }
 
-# output "psql_cluster_connection_params" {
-#   description = "Connection parameters of the managed PostgreSQL cluster"
-#   value = {
-#     "host" : "c-${yandex_mdb_postgresql_cluster.givegift_psql_cluster.id}.rw.mdb.yandexcloud.net",
-#     "port" : 6432,
-#     "dbname" : yandex_mdb_postgresql_database.givegift_psql_db.name,
-#     "user_name" : yandex_mdb_postgresql_user.givegift_psql_user.name
-#   }
-# }
+output "psql_cluster_connection_params" {
+  description = "Connection parameters of the managed PostgreSQL cluster"
+  value = {
+    "host" : "c-${yandex_mdb_postgresql_cluster.givegift_psql_cluster.id}.rw.mdb.yandexcloud.net",
+    "port" : 6432,
+    "dbname" : yandex_mdb_postgresql_database.givegift_psql_db.name,
+    "user_name" : yandex_mdb_postgresql_user.givegift_psql_user.name
+  }
+}
 
 output "k8s_cluster_name" {
   description = "Name of the managed Kubernetes cluster"
